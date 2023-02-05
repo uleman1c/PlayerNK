41e8f3150ed401329c1dd567672c55c7ae097c60
Mihail Ulyanenkov <uleman@mail.ru>
2023-02-05 19:27:37 +0300

    exeption

--- app/src/main/java/com/example/playernk/FirstFragment.java
+++ app/src/main/java/com/example/playernk/FirstFragment.java
@@ -348,7 +348,7 @@ public class FirstFragment extends Fragment {
 
 
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            //throw new RuntimeException(e);
         }
     }
 
