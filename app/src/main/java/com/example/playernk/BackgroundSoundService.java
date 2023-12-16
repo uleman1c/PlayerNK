02a5967db86c324e0af40f9cac1e0704174e2bb3
Mihail <uleman@mail.ru>
2023-12-16 12:24:59 +0300

    BackgroundSoundService start

---
+++ app/src/main/java/com/example/playernk/BackgroundSoundService.java
@@ -0,0 +1,51 @@
+package com.example.playernk;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class BackgroundSoundService extends Service {
+
+    MediaPlayer mediaPlayer;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mediaPlayer = new MediaPlayer();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mediaPlayer.setAudioAttributes(
+                    new AudioAttributes.Builder()
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .build()
+            );
+        }
+
+
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
