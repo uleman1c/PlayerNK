bb04b1b9e64ebf4cf7caea9fc747050843661d2b
Mihail <uleman@mail.ru>
2023-12-17 17:30:56 +0300

    dreaman

--- app/src/main/java/com/example/playernk/Conn.java
+++ app/src/main/java/com/example/playernk/Conn.java
@@ -2,7 +2,7 @@ package com.example.playernk;
 
 public class Conn {
 
-    //public static String addr = "http://10.0.2.2:3001/";
+    //public static String addr = "https://dev.dreamantica.ru/srv/";
     public static String addr = "http://188.120.243.243:3001/";
 
 }
